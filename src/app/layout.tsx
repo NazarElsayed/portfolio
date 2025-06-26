54795a982f5a04e897db5cd4efe927ab04677479
Nazar Elsayed <nazar.elsayed@gmail.com>
2025-06-26 04:38:53 +0100

    Fixed Tailwind and Simplified Styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
 import TopNav from "@/app/ui/TopNav";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Nazar Elsayed | Portfolio",
@@ -25,12 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <TopNav />
-      {children}
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <body className={`antialiased`}>
+        <TopNav />
+        {children}
       </body>
     </html>
   );
